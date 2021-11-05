bc6fe9df195349f42b8958b7c38f09405356d3fc
Ryan <ryangabriel.angelo@gmail.com>
2021-11-05 11:54:36 -0300

    ExemploAngular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ProdutosComponent } from './views/produtos/produtos.component';
+
+const routes: Routes = [
+  { path: "", component: HomeComponent},
+  { path: "produtos", component: ProdutosComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
